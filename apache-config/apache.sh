51ed5303dddceebac51a570be4100522c527ea3c
rmmenezes <ra29fa@gmail.com>
2022-10-31 15:24:33 -0300

    .

---
+++ apache-config/apache.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install apache2 -y
+
+sudo mkdir /var/www/cursoceape
+
+sudo cp index.html /var/www/cursoceape/index.html
+#sudo chown rmba:rmba /var/www/cursoceape/index.html
+sudo chmod -R 755 /var/www/cursoceape/
+
+
+sudo cp cursoceape.conf /etc/apache2/sites-available/cursoceape.conf 
+#sudo chown rmba:rmba /var/www/cursoceape/index.html
+sudo chmod -R 755 /etc/apache2/sites-available/cursoceape.conf
+
+sudo a2ensite cursoceape.conf 
+sudo a2dissite 000-default.conf
+
+#sudo bash -c 'echo "ServerName localhost" >> /etc/apache2/apache2.conf'
+
+sudo apache2ctl configtest 
+sudo systemctl enable apache2
+sudo systemctl restart apache2
+
+sudo ufw allow 80/tcp 
+sudo ufw allow 443/tcp
