e25f9d228dfe345c09e190bd2f6b96095c53fc23
Rafael Menezes Barboza <ra29fa@gmail.com>
2022-11-07 11:22:42 -0300

    Update apache.sh

--- apache-config/apache.sh
+++ apache-config/apache.sh
@@ -6,19 +6,19 @@ sudo apt-get install apache2 -y
 
 sudo mkdir /var/www/cursoceape
 
-sudo cp index.html /var/www/cursoceape/index.html
-#sudo chown rmba:rmba /var/www/cursoceape/index.html
+sudo cp index.html /var/www/cursoceape/index.php
+#sudo chown rmba:rmba /var/www/cursoceape/index.php
 sudo chmod -R 755 /var/www/cursoceape/
 
 
 sudo cp cursoceape.conf /etc/apache2/sites-available/cursoceape.conf 
-#sudo chown rmba:rmba /var/www/cursoceape/index.html
+#sudo chown rmba:rmba /var/www/cursoceape/index.php
 sudo chmod -R 755 /etc/apache2/sites-available/cursoceape.conf
 
 sudo a2ensite cursoceape.conf 
 sudo a2dissite 000-default.conf
 
-#sudo bash -c 'echo "ServerName localhost" >> /etc/apache2/apache2.conf'
+sudo bash -c 'echo "ServerName localhost" >> /etc/apache2/apache2.conf'
 
 sudo apache2ctl configtest 
 sudo systemctl enable apache2
